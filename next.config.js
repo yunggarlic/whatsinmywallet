7e5fab4cb22d832ec7833bb1652a18b7b618e936
Tim Ferrari <ferrari.tim.v@gmail.com>
2023-02-16 20:39:11 -0600

    init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+// setting up Meshjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: function (config, options){
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    return config;
+  }
 }
 
 module.exports = nextConfig
