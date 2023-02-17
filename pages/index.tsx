021beac1bd4b5de4eb7772f2c394a2a03b8d8e41
Tim Ferrari <ferrari.tim.v@gmail.com>
2023-02-16 21:21:14 -0600

    adds dashboard and pushes url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import type { NextPage } from "next";
-import { useWallet } from '@meshsdk/react';
-import { CardanoWallet } from '@meshsdk/react';
+import { useState, useEffect } from "react";
+import type { NextPage, Link } from "next";
+import { useWallet } from "@meshsdk/react";
+import { CardanoWallet } from "@meshsdk/react";
 
 const Home: NextPage = () => {
   const { connected, wallet } = useWallet();
@@ -17,34 +17,43 @@ const Home: NextPage = () => {
     }
   }
 
+  useEffect(() => {
+    //update url
+    if (connected) {
+      window.history.pushState({}, "", "/dashboard");
+    }
+  }, [connected])
+
   return (
-    <div>
-      <h1>Connect Wallet</h1>
-      <CardanoWallet />
-      {connected && (
-        <>
-          <h1>Get Wallet Assets</h1>
-          {assets ? (
-            <pre>
-              <code className="language-js">
-                {JSON.stringify(assets, null, 2)}
-              </code>
-            </pre>
-          ) : (
-            <button
-              type="button"
-              onClick={() => getAssets()}
-              disabled={loading}
-              style={{
-                margin: "8px",
-                backgroundColor: loading ? "orange" : "grey",
-              }}
-            >
-              Get Wallet Assets
-            </button>
-          )}
-        </>
-      )}
+    <div className="connect-wallet">
+      <div>
+        <h1>Connect Wallet</h1>
+        <CardanoWallet />
+        {connected && (
+          <>
+            <h1>Get Wallet Assets</h1>
+            {assets ? (
+              <pre>
+                <code className="language-js">
+                  {JSON.stringify(assets, null, 2)}
+                </code>
+              </pre>
+            ) : (
+              <button
+                type="button"
+                onClick={() => getAssets()}
+                disabled={loading}
+                style={{
+                  margin: "8px",
+                  backgroundColor: loading ? "orange" : "grey",
+                }}
+              >
+                Get Wallet Assets
+              </button>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
